dce18460ed2e6097d1e76be6ff518ea333ee942a
tomjadams <tomjadams@06e54ded-fb4f-0410-92ca-a3b3d71ef8f1>
2008-08-19 06:27:39 +0000

    Working on parsing using an iterator.

--- trunk/src/main/scala/com/googlecode/furnace/Main.scala
+++ trunk/src/main/scala/com/googlecode/furnace/Main.scala
@@ -1,14 +1,11 @@
 package com.googlecode.furnace
 
-import parse.{FastaParser}
-import java.io.{File, FileInputStream}
+import file.io.FilePath
 import file.io.FilePath._
-import file.io.ReadableFile._
-import file.io.{FilePath, ReadableFile}
-import java.lang.Integer.parseInt
-import sequence.GeneSequence._
 
 object SequenceSearcher {
+  import java.lang.Integer.parseInt
+
   val defaultSliceSize = 40
 
   def main(args: Array[String]): Unit = args match {
@@ -19,44 +16,30 @@ object SequenceSearcher {
 }
 
 object Main {
-  import scalaz.io.File
-  import scalaz.io.File._
+  import java.io.{File, FileInputStream}
   import scalaz.javas.InputStream._
-  import sequence.Base
-  import sequence.Base._
   import sequence.GeneSequence
   import sequence.GeneSequence._
   import parse.FastaParser._
 
   def run(inputSequence: FilePath, sliceSize: Int) {
     println("Processing sequence file: " + filePathToString(inputSequence) + ", slice size: " + sliceSize)
-    val sequences = parse(new FileInputStream(inputSequence: File), sliceSize).toList
-    println(sequences.size + " sequences")
-    sequences.foreach(println)
-    //    val completeSequences = sequences.filter(_.sequence.size == sliceSize)
-    //    println("Found " + sequences.size + " sequences in input file, discarding " + (sequences.size - completeSequences.size) + " partial trailing sequence")
-    //    println(completeSequences.size + " complete sequences")
-    //    completeSequences.foreach(println)
-    println("Fin.")
+    val result = parse(new FileInputStream(inputSequence), sliceSize)
+    if (result.isEmpty) {
+      println("No sequences found in source file")
+    } else {
+      val sequences = result.get
+      while (sequences.hasNext) {
+        println(sequences.next())
+      }
+//      println(sequences.get.toList.size + " sequences")
+//      sequences.foreach(println)
+      //    val completeSequences = sequences.filter(_.sequence.size == sliceSize)
+      //    println("Found " + sequences.size + " sequences in input file, discarding " + (sequences.size - completeSequences.size) + " partial trailing sequence")
+      //    println(completeSequences.size + " complete sequences")
+      //    completeSequences.foreach(println)
+      println("Fin.")
+
+    }
   }
-
-
-//
-//  def foo(f: File, sliceSize: Int) = {
-//    //    f.read(Nothing, byteToBase)
-//    //    val sequence = f.read(0, streamToGeneSequence)
-//    val sequence = f.read(Nil: GeneSequence, xxx)
-//    List(sequence)
-//  }
-//
-//  def xxx(g: GeneSequence, b: Byte): GeneSequence = g
-//
-//  lazy val newLines = List('\n', '\r')
-//
-//  def streamToGeneSequence(s: Stream[Byte]): GeneSequence = geneSequence(readSequence(s, 40).foldLeft(Nil: List[Base])(_ + byteToBase(_)))
-//
-//  // TODO Need to take and drop from the stream
-//  private def readSequence(s: Stream[Byte], howMany: Int): Stream[Byte] = removeHeader(s).filter(!newLines.contains(_)).take(howMany)
-//
-//  private def removeHeader(s: Stream[Byte]) = s
 }
