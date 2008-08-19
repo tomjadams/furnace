5211f2954f77cd61cf01abe36c3d1c178f8ec8de
tomjadams <tomjadams@06e54ded-fb4f-0410-92ca-a3b3d71ef8f1>
2008-08-19 04:58:42 +0000

    Working on parsing using an iterator.

--- src/main/scala/com/googlecode/furnace/parse/FastaParser.scala
+++ src/main/scala/com/googlecode/furnace/parse/FastaParser.scala
@@ -1,6 +1,7 @@
 package com.googlecode.furnace.parse
 
 import scala.collection.mutable.{ArrayBuffer => MutableArray}
+import scalaz.list.NonEmptyList, NonEmptyList._
 import sequence.{Base, GeneSequence}
 import sequence.Base._
 import sequence.GeneSequence._
@@ -8,20 +9,24 @@ import sequence.GeneSequence._
 object FastaParser {
   private lazy val newLines = List('\n', '\r')
 
-  def parse(input: Iterator[Byte], sliceSize: Int): Iterator[GeneSequence] = new Iterator[GeneSequence] {
+  def parse(input: Iterator[Byte], sliceSize: Int): Option[Iterator[GeneSequence]] =
+    if (input.hasNext) {
+      Some(new GeneSequenceIterator(input))
+    } else {
+      None
+    }
+
+  private def read(limit: Int, current: Int): Seq[Byte] = error("Not baked yet")
+
+  private final class GeneSequenceIterator(input: Iterator[Byte]) extends Iterator[GeneSequence] {
     def hasNext = input.hasNext
 
-    def next =
-      if (input.hasNext) {
-        val bases = new MutableArray[Byte]
-        val base = input.next
-        bases += base
-//        println(">>> Base: " + base)
-        geneSequence(bases)
-      } else {
-        emptySequence
-      }
-
-    private def read(limit: Int, current: Int): Seq[Byte] = error("")
+    def next = {
+      val bases = new MutableArray[Base]
+      val base = input.next
+      bases += base
+      //        println(">>> Base: " + base)
+      geneSequence(list(bases.toList))
+    }
   }
 }
