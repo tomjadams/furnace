6b3a6522eb7e0b6ab0c036ff2a56d3991052b5a5
tomjadams <tomjadams@06e54ded-fb4f-0410-92ca-a3b3d71ef8f1>
2008-08-21 03:19:05 +0000

    BLAST!!!

---
+++ trunk/src/main/scala/com/googlecode/furnace/analyse/BlastAnalysisResult.scala
@@ -0,0 +1,5 @@
+package com.googlecode.furnace.analyse
+
+import file.io.FilePath
+
+final case class BlastAnalysisResult(analysisOutput: FilePath) extends AnalysisResult
---
+++ trunk/src/main/scala/com/googlecode/furnace/invoke/LocalBlastInvoker.scala
@@ -0,0 +1,7 @@
+package com.googlecode.furnace.invoke
+
+import sequence.GeneSequence
+
+final class LocalBlastInvoker extends Invoker {
+  def invoke(sequence: GeneSequence) = error("TODO")
+}
