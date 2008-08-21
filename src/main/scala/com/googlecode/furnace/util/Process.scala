22612dbd633f1d3ac0f8045a60075715ef8628bc
tomjadams <tomjadams@06e54ded-fb4f-0410-92ca-a3b3d71ef8f1>
2008-08-21 06:20:16 +0000

    Command line runner

--- src/main/scala/com/googlecode/furnace/util/Process.scala
+++ src/main/scala/com/googlecode/furnace/util/Process.scala
@@ -1,11 +1,9 @@
 package com.googlecode.furnace.util
 
-final case class Process(p: java.lang.Process) {
-  import scalaz.javas.InputStream.InputStreamByteStream
-
-  def output = p.getInputStream.map(_.toChar).mkString
-}
-
 object Process {
-  implicit def processToProcess(p: java.lang.Process): Process = Process(p)
+  implicit def pimpedProcess(p: java.lang.Process) = new {
+    import scalaz.javas.InputStream.InputStreamByteStream
+
+    def output = p.getInputStream.map(_.toChar).mkString
+  }
 }
